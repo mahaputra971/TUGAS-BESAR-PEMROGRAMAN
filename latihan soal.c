2f4762f1e1e97dd9b2061ab7d5081883fdd29f09
Matthew Dharma <76445633+matthewdharma@users.noreply.github.com>
2020-12-28 15:32:46 +0800

    Add files via upload

---
+++ latihan soal.c
@@ -0,0 +1,318 @@
+#include <stdio.h>
+
+int main()
+{
+int JawabanPilihan;
+printf("\nSOAL LATIHAN PERHITUNGAN LUAS PERMUKAAN BANGUN RUANG\n\n");
+//nomor 1//
+printf("1.)  Luas permukaan bola yang berdiameter 21 cm dengan phi=22/7 adalah...cm2.");
+printf("\n     a.) 264");
+printf("\n     b.) 462");
+printf("\n     c.) 1.386");
+printf("\n     d.) 4.851");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='c')
+    {printf("Benar, jawabannya adalah 1.386 cm2.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 2//
+printf("\n\n2.)  Sebuah kubus memiliki panjang rusuk 20 cm, luas permukaan kubus adalah...cm2.");
+printf("\n     a.) 180");
+printf("\n     b.) 5.400");
+printf("\n     c.) 10.800");
+printf("\n     d.) 2.700");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='b')
+    {printf("Benar, jawabannya adalah 5.400 cm2.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 3//
+printf("\n\n3.)  Sebuah prisma alasnya berbentuk segitiga siku-siku. Jika panjang sisinya 6 cm, 8 cm, serta 10 cm, dan tinggi prisma tersebut adalah 15 cm,");
+printf("\n     maka luas permukaan prisma tersebut...cm2.");
+printf("\n     a.) 398");
+printf("\n     b.) 404");
+printf("\n     c.) 412");
+printf("\n     d.) 408");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='d')
+    {printf("Benar, jawabannya adalah 408 cm2.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 4//
+printf("\n\n4.)  Sebuah limas dengan alas dan semua sisi tegaknya berbentuk segitiga sama sisi dengan panjang sisinya 8 cm, luas permukaan limas...cm2.");
+printf("\n     a.) 64 akar 3");
+printf("\n     b.) 48 akar 3");
+printf("\n     c.) 16 akar 3");
+printf("\n     d.) 32 akar 3");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='a')
+    {printf("Benar, jawabannya adalah 64 akar 3 cm2.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 5//
+printf("\n\n5.)  Sebuah balok dengan panjang 12 cm, lebar 15 cm, tinggi 30 cm, Luas permukaan balok adalah...cm2.");
+printf("\n     a.) 2.035");
+printf("\n     b.) 1.980");
+printf("\n     c.) 1.350");
+printf("\n     d.) 3.000");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='b')
+    {printf("Benar, jawabannya adalah 1.980 cm2.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+
+
+printf("\n\nSOAL LATIHAN PERHITUNGAN MODUS, MEDIAN, DAN MEAN\n\n");
+//nomor 1//
+printf("1.)  Hasil panen kakek selama 5 bulan dalam ton adalah 10, 6, 7, 9, 8. Rata-rata hasil panen kakek tiap bulan adalah...ton.");
+printf("\n     a.) 7");
+printf("\n     b.) 8");
+printf("\n     c.) 9");
+printf("\n     d.) 10");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='b')
+    {printf("Benar, jawabannya adalah 8 ton.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 2//
+printf("\n\n2.)  Pelemparan dadu sebanyak 25 kali. Angka yang keluar datanya adalah:");
+printf("\n     1 2 3 4 5 5 6 2 3 4 5 6 6 4 3 2 1 4 3 5 6 6 5 4 5");
+printf("\n     Modus dari data di atas adalah...");
+printf("\n     a.) 3");
+printf("\n     b.) 4");
+printf("\n     c.) 5");
+printf("\n     d.) 6");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='c')
+    {printf("Benar, jawabannya adalah 5.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 3//
+printf("\n\n3.)  Hasil ulangan matematika 21 anak kelas VI adalah:");
+printf("\n     7, 8, 9, 8, 7, 8, 10, 9, 5, 7, 9, 9, 8, 7, 10, 8, 9, 6, 8, 7, dan 5.");
+printf("\n     Apabila data tersebut diurutkan dari terkecil hingga terbesar, maka nilai tengah dari data tersebut adalah...");
+printf("\n     a.) 6");
+printf("\n     b.) 7");
+printf("\n     c.) 8");
+printf("\n     d.) 9");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='c')
+    {printf("Benar, jawabannya adalah 8.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 4//
+printf("\n\n4.)  Data hasil ulangan Bahasa Indonesia Linda sebanyak 4 kali yaitu 10, 8, 9, 6");
+printf("\n     Nilai rata-rata yang diperoleh Linda adalah...");
+printf("\n     a.) 7.50");
+printf("\n     b.) 8.25");
+printf("\n     c.) 8.50");
+printf("\n     d.) 9.25");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='b')
+    {printf("Benar, jawabannya adalah 8.25.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 5//
+printf("\n\n5.)  Ibu membeli gula 8 kg, jagung 10 kg, beras 15 kg, kedelai 12 kg dan kentang 5 kg.");
+printf("\n     Berat rata-rata belanjaan ibu adalah...");
+printf("\n     a.) 7 kg");
+printf("\n     b.) 8 kg");
+printf("\n     c.) 9 kg");
+printf("\n     d.) 10 kg");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='d')
+    {printf("Benar, jawabannya adalah 10 kg.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+
+
+printf("\n\nSOAL LATIHAN PERHITUNGAN FAKTORIAL, PERMUTASI, DAN KOMBINASI\n\n");
+//nomor 1//
+printf("1.)  Hasil dari 7! adalah...");
+printf("\n     a.) 5.040");
+printf("\n     b.) 3.440");
+printf("\n     c.) 4.240");
+printf("\n     d.) 2.540");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='a')
+    {printf("Benar, jawabannya adalah 5.040.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 2//
+printf("\n\n2.)  Seorang satpam bank ingin mencetak nomor antrian nasabah yang terdiri dari tiga angka.");
+printf("\n     Jika nomor antrian tersebut tidak memuat angka yang sama yang dibentuk dari angka 0, 1, 2, 3.");
+printf("\n     Banyak pilihan nomor antrian yang dapat dibuat adalah...");
+printf("\n     a.) 4 cara");
+printf("\n     b.) 12 cara");
+printf("\n     c.) 24 cara");
+printf("\n     d.) 36 cara");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='c')
+    {printf("Benar, jawabannya adalah 24 cara.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 3//
+printf("\n\n3.)  Setiap tahun, SMA Pelita Bangsa selalu mengadakan pentas seni. Sebelum acara akbar,");
+printf("\n     para siswa mengadakan pemilihan ketua, sekretaris dan bendahara. Setelah melakukan seleksi,ada 5 orang siswa yang mendaftarkan diri.");
+printf("\n     ada 5 orang siswa yang mendaftarkan diri.");
+printf("\n     Banyak cara untuk memilih ketua, sekretaris dan bendahara untuk acara tersebut adalah...");
+printf("\n     a.) 720 cara");
+printf("\n     b.) 360 cara");
+printf("\n     c.) 120 cara");
+printf("\n     d.) 60 cara");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='d')
+    {printf("Benar, jawabannya adalah 60 cara.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 4//
+printf("\n\n4.)  Sebuah kantong berisi 6 kelereng putih, 4 kelereng biru dan 3 kelereng merah.");
+printf("\n     Banyak cara pengambilan 3 kelereng putih dari kantong tersebut adalah...");
+printf("\n     a.) 360 cara");
+printf("\n     b.) 120 cara");
+printf("\n     c.) 60 cara");
+printf("\n     d.) 20 cara");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='d')
+    {printf("Benar, jawabannya adalah 20 cara.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 5//
+printf("\n\n5.)  Seorang peternak akan membeli hewan ternak untuk dipelihara. Dia akan membeli 3 ekor sapi, 4 ekor domba dan 5 ekor kambing.");
+printf("\n     Seorang pedagang mempunyai 6 ekor sapi, 6 ekor domba dan 8 ekor kambing.");
+printf("\n     Banyak cara yang dapat dilakukan untuk memilih hewan ternak yang akan dibeli adalah...");
+printf("\n     a.) 16.800 cara");
+printf("\n     b.) 9.000 cara");
+printf("\n     c.) 300 cara");
+printf("\n     d.) 120 cara");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='a')
+    {printf("Benar, jawabannya adalah 16.800 cara.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+
+
+printf("\n\nSOAL LATIHAN PERHITUNGAN KONVERSI SATUAN\n\n");
+//nomor 1//
+printf("1.)  2 km - 1200 m + 3 hm =...m");
+printf("\n     a.) 1.100 m");
+printf("\n     b.) 2.510 m");
+printf("\n     c.) 1.205 m");
+printf("\n     d.) 2.100 m");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='a')
+    {printf("Benar, jawabannya adalah 1.100 m.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 2//
+printf("\n\n2.)  50 hm + 3 km - 420 m =...dm");
+printf("\n     a.) 35.400 dm");
+printf("\n     b.) 43.200 dm");
+printf("\n     c.) 54.500 dm");
+printf("\n     d.) 75.800 dm");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='d')
+    {printf("Benar, jawabannya adalah 75.800 dm.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 3//
+printf("\n\n3.)  tentukanlah 4 km =...m");
+printf("\n     a.) 40 m");
+printf("\n     b.) 400 m");
+printf("\n     c.) 4.000 m");
+printf("\n     d.) 0.4 m");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='a')
+    {printf("Benar, jawabannya adalah 40 m.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 4//
+printf("\n\n4.)  Nilai dari 100 m =...cm");
+printf("\n     a.) 10.000 cm");
+printf("\n     b.) 1.000 cm");
+printf("\n     c.) 100 cm");
+printf("\n     d.) 10 cm");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='a')
+    {printf("Benar, jawabannya adalah 10.000 cm.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 5//
+printf("\n\n5.)  Nilai dari 5 dam =...dm");
+printf("\n     a.) 50 dm");
+printf("\n     b.) 500 dm");
+printf("\n     c.) 5.000 dm");
+printf("\n     d.) 50.000 dm");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='b')
+    {printf("Benar, jawabannya adalah 500 dm.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+
+
+printf("\n\nSOAL LATIHAN PERHITUNGAN BUNGA\n\n");
+//nomor 1//
+printf("1.)  Ibu menabung di bank, setoran awalnya Rp1.000.000,00 dengan bunga 10 persen/tahun.");
+printf("\n     Berapa total tabungan ibu pada tahun ketiga?");
+printf("\n     a.) 13.000.000");
+printf("\n     b.) 1.300.000");
+printf("\n     c.) 1.030.000");
+printf("\n     d.) 1.003.000");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='b')
+    {printf("Benar, jawabannya adalah 1.300.000.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 2//
+printf("\n\n2.)  Dina menabung di bank, setoran awalnya Rp300.000,00 dengan bunga 5 persen/tahun.");
+printf("\n     Berapakah bunga yang dina peroleh selama 6 bulan?");
+printf("\n     a.) 30.000");
+printf("\n     b.) 15.000");
+printf("\n     c.) 7.500");
+printf("\n     d.) 2.500");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='c')
+    {printf("Benar, jawabannya adalah 7.500.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 3//
+printf("\n\n3.)  Rian menabung di bank sebesar Rp400.000,00 dengan suku bunga tunggal 9 persen/tahun.");
+printf("\n     Tabungan Rian saat diambil sebesar Rp460.000,00.");
+printf("\n     Berapa lama Rian menabung di bank tersebut?");
+printf("\n     a.) 2 tahun");
+printf("\n     b.) 1 tahun 10 bulan");
+printf("\n     c.) 2 tahun 2 bulan");
+printf("\n     d.) 1 tahun 8 bulan");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='d')
+    {printf("Benar, jawabannya adalah 1 tahun 8 bulan.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 4//
+printf("\n\n4.)  Ridwan menabung di bank, dengan bunga 5 persen/tahun.");
+printf("\n     Setelah 6 bulan uang Ridwan bertambah Rp62.500,00.");
+printf("\n     Berapakah setoran awal Ridwan?");
+printf("\n     a.) 2.500.000");
+printf("\n     b.) 625.000");
+printf("\n     c.) 6.250.000");
+printf("\n     d.) 2.000.000");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='a')
+    {printf("Benar, jawabannya adalah 2.500.000.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+//nomor 5//
+printf("\n\n5.)  Kiki menabung di bank Rp150.000,00 dengan bunga 12 persen/tahun.");
+printf("\n     arena ada kebutuhan keluarga, uang itu diambil seluruhnya dengan bunganya sebesar Rp165.000,00.");
+printf("\n     Berapa lama Kiki menabung?");
+printf("\n     a.) 5 bulan");
+printf("\n     b.) 10 bulan");
+printf("\n     c.) 15 bulan");
+printf("\n     d.) 20 bulan");
+printf("\n     Jawaban: ");scanf(" %c", &JawabanPilihan);
+if (JawabanPilihan=='b')
+    {printf("Benar, jawabannya adalah 10 bulan.");}
+else
+{printf("Salah, silahkan pelajari materi lagi.");}
+}
+
